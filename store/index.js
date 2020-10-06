be41bb73bcf3db59317a4f5f9de73038b7a4a0b6
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-05 22:42:43 -0500

    updates after class

---
+++ store/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as Bio } from "./Bio";
+export { default as Gallery } from "./Gallery";
+export { default as Form } from "./Forms";
+export { default as Links } from "./Links";
