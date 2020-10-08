535efcc5e737be2f367146fd0822366f0079dc2c
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-07 20:17:18 -0500

    html look

--- components/views/Blog.js
+++ components/views/Blog.js
@@ -2,3 +2,17 @@ export default st => `
 <section id="blog">
 ${st.posts.map(post => {}).join()}
 </section>`;
+
+function formatBlogPost(post) {
+  return `
+  <div class="blog-post">
+    <h4>${post.title} by User ${post.userId}</h4>
+    <p>${post.body}</p>
+  </div>`;
+}
+export default st => `
+<section id="blog">
+${st.posts.map(post => {
+  formatBlogPost(post)
+}).join()}
+</section>`
