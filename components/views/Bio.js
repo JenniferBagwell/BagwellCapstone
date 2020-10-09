1feba84e50d9c3ae7072307c651fdba696617d92
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-08 19:14:55 -0500

    bio.js

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,8 +1,43 @@
 export default () => `
 <section id="bio">
-    <h2>Vivamus ac justo eu nisi</h2>
-    <img src="https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="me">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sagittis nulla. Etiam semper mauris a quam tempus, non feugiat massa posuere. Morbi vitae nisi aliquam, semper diam id, molestie leo. In hac habitasse platea dictumst. Nulla et enim vel elit dictum viverra. Nunc pharetra lacinia arcu sit amet dignissim. Pellentesque ut mi nulla.</p>
-    <p>Quisque at hendrerit libero, eget interdum lectus. Etiam facilisis leo nulla, sit amet imperdiet nunc molestie vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur consectetur felis a purus volutpat, sed finibus magna iaculis. Pellentesque tristique tristique turpis nec vehicula. Maecenas varius quis tellus id mollis. Vivamus ut ultrices ligula.</p>
-    <p>Etiam egestas consectetur gravida. Nulla mollis suscipit sapien sed fermentum. Integer vitae eros a magna vulputate aliquam. Suspendisse sed pulvinar augue, auctor mollis lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis eleifend diam quis libero sollicitudin efficitur. Nullam sapien eros, tempor eget vulputate ut, interdum vel orci. Donec sit amet tempor mi. Nam feugiat cursus egestas. Suspendisse eget orci et ex mattis ornare tempor non tellus. Suspendisse gravida neque in urna congue bibendum. Duis dui odio, pharetra nec odio ac, ornare vulputate nibh.</p>
+    <h2>My Bio</h2>
+    <!DOCTYPE html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="Coder Bio" content="width=device-width">
+    <header>
+    <title>Procode Bio</title>
+  </header>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+    <h1>Professional Link Biohub</h1>
+    <script src="index.js"></script>
+    <body>
+<h2 class="purple">Professional Bio Linkhub</h2>
+<br>
+<p>This is my professional savvycoders bio. From you can you access my savvycoder profiles through the links provided. There is also a list of all my currant professional Certifications. This professional coder bio will be updated routinely as I continue to build my coding career.</p>
+<h3>My professional coding links</h3>
+<ol><h4>
+  <li><a href="https://github.com/JenniferBagwell"> My GitHub </a></li>
+  <li><a href="https://trello.com/b/yCWNgWGD/bagwell-capstone-kanban"> My Trello Board </a></li>
+  </h4>
+</ol>
+<h3 id="inbox">My Professional Certifications</h3>
+<ol><h4>
+  <li><h3>Agile Certifications</h3></li>
+  <img src="imgs/agilecert.png" alt="Agile Certification" width="30%" height="auto">
+  <h3><li>Advanced Social Media Certification</h3></li>
+  <img src="imgs/social media cert.png" alt="Agile Certification" width="30%" height="auto">
+
+</h4></ol>
+
+    </body>
+    <footer>
+This is the footer section
+    </footer>
+  </body>
+</html>
+</p>
   </section>`;
