67bb5e633e06f2eb7b300cf78f9907476beaa288
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-08 19:39:36 -0500

    color fix

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,6 +1,6 @@
 export default () =>
-<section id="bio">
 
+<section id="bio">
     <h2>My Bio</h2>
     <!DOCTYPE html>
 <html>
@@ -16,7 +16,7 @@ export default () =>
     <h1>Professional Link Biohub</h1>
     <script src="index.js"></script>
     <body>
-<h2 class="white">Professional Bio Linkhub</h2>
+<h2 class="purple">Professional Bio Linkhub</h2>
 <br>
 <p>This is my professional savvycoders bio. From you can you access my savvycoder profiles through the links provided. There is also a list of all my currant professional Certifications. This professional coder bio will be updated routinely as I continue to build my coding career.</p>
 <h3>My professional coding links</h3>
