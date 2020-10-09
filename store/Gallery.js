a05acbab328269615fe2a072081e8ea283385714
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-08 20:20:07 -0500

    gallery

--- store/Gallery.js
+++ store/Gallery.js
@@ -3,12 +3,13 @@ export default {
   view: "Gallery",
   pictures: [
     {
-      url: "img/agilecert.png",
+      url: "https://cdn.filestackcontent.com/KHQdRsg4TTmFRX8WYSQ9",
       title: "Agile Cert"
     },
     {
-      url: "img/social media cert.png",
-      title: "Social Media Cert"
+      url:
+        "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      title: "mlep"
     }
   ]
 };
