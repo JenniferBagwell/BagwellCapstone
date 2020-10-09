1a3d85c2904042326f7dc34902116f91e1258c80
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-08 20:03:05 -0500

    ahhhh

--- components/views/Gallery.js
+++ components/views/Gallery.js
@@ -1,6 +1,5 @@
 export default st => `
 ${st.pictures.reduce(
-  (html, pic) =>
-    html + `<img src="${img / procodebackground.png}" alt="${pic.title}">`,
+  (html, pic) => html + `<img src="${pic.url}" alt="${pic.title}">`,
   ``
 )}`;
