0bbad58eeaf98a01ffb0341d00d2cade9b21eba0
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-16 00:04:50 -0500

    gallery fix

--- store/Gallery.js
+++ store/Gallery.js
@@ -7,9 +7,8 @@ export default {
       title: "Agile Cert"
     },
     {
-      url:
-        "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-      title: "mlep"
+      url: "img url here",
+      title: "to be added"
     }
   ]
 };
