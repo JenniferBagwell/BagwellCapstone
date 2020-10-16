ecd0c84b269544daaecea2057ad33b8212d75df2
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-16 00:00:45 -0500

    reverse it cause it broke

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,5 @@
 export default st => `
 <header>
-.header-image #title a {
-  background: url( 'ProCodeBio.mp4' ) no-repeat;
-}
   <h1>${st.header}</h1>
   </header>
   `;
--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,5 @@
 export default () => `
 <section id="csstransfer">
   <h2>ProCode Bio</h2>
-  <video width="320" height="240">
-  <source src="imgs/ProCodeBio" type="video/mp4"></video>
   <im href="">More To Come</source>
 </section>`;
