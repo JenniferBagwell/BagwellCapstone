eed43050cc7499dfcd8351b3f8d0063e33a23fe3
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-26 20:01:46 -0500

    link updates

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -8,8 +8,9 @@ export default () => `
 <p>This is my professional savvycoders bio. From you can you access my savvycoder profiles through the links provided. There is also a list of all my currant professional Certifications. This professional coder bio will be updated as I continue to build my coding career.</p>
 <h3>My professional coding links</h3>
 <ol><h4>
-<li><a href="https://github.com/JenniferBagwell"> My LinkedIn </a></li>
-<li><a href="https://github.com/JenniferBagwell"> Facebook </a></li>
+<li><a href="https://www.linkedin.com/in/jenniferbagwell808/"> My LinkedIn </a></li>
+<li><a href="https://www.facebook.com/JennDawB"> Facebook </a></li>
+<li><a href="https://www.supportingstrategies.com/bootcamp-slm19-jennifer-bagwell">Bootcamp Session</li>
   <li><a href="https://github.com/JenniferBagwell"> My GitHub </a></li>
   <li><a href="https://trello.com/b/yCWNgWGD/bagwell-capstone-kanban"> My Trello Board </a></li>
   <li><a href="https://github.com/JenniferBagwell"> Previous Contract Work </a></li>
