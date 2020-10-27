ffc491d9cb762478bf3dc828f32a0d35fa436d92
JenniferBagwell <jdawbagwell@gmail.com>
2020-10-26 20:30:21 -0500

    blog

--- components/views/Blog.js
+++ components/views/Blog.js
@@ -1,6 +1,26 @@
 import { capitalize } from "lodash";
 export default st => `
 <section id="blog">
+<form
+  action="https://formspree.io/f/xeqpqlvq"
+  method="POST"
+>
+  <label>
+  Contact me for more info
+  </label>
+  <label>
+    Your email:
+    <input type="text" name="_replyto">
+  </label>
+  <label>
+    Your message:
+    <textarea name="message"></textarea>
+  </label>
+
+  <!-- your other form fields go here -->
+
+  <button type="submit">Send</button>
+</form> `;
 ${st.posts
   .map(post => {
     return formatBlogPost(post);
