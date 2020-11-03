79f98392db390e34e181538a8b2203a5d7356a9b
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-02 21:46:31 -0600

    server data

---
+++ components/views/Blog1.js
@@ -0,0 +1,18 @@
+import { capitalize } from "lodash";
+export default st => `
+<section id="blog">
+
+${st.posts
+  .map(post => {
+    return formatBlogPost(post);
+  })
+  .join()}
+</section>`;
+
+function formatBlogPost(post) {
+  return `
+  <div class="blog-post">
+    <h4>${capitalize(post.title)} by User ${post.userid}</h4>
+    <p>${capitalize(post.post)}</p>
+  </div>`;
+}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+
+mongoose.connect("mongodb://localhost/test");
 
 app.use(cors());
 app.use(express.json());
