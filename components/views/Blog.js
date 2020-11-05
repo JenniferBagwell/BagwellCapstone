596abbe598ed779e3b54199a595db23f6bc1b32b
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-04 21:04:44 -0600

    api weather blog

--- components/views/Blog.js
+++ components/views/Blog.js
@@ -1,25 +1,13 @@
-import { capitalize } from "lodash";
 export default st => `
-<section id="blog">
-<h4>Weather in ${st.weather.city}: ${
+<h3>Weather in ${st.weather.city}: ${
   st.weather.description
 } // ${kelvinToFahrenheit(st.weather.temp)}F, feels like ${kelvinToFahrenheit(
   st.weather.feelsLike
-)}F</h4>
-
-${st.posts
-  .map(post => {
-    return formatBlogPost(post);
-  })
-  .join()}
+)}F</h3>
+<section id="csstransfer">
+  <h2>Procode Bio Blog</h2>
+  <a href="">"Sign up for alerts" "Button"</a>
 </section>`;
 
-function formatBlogPost(post) {
-  return `
-  <div class="blog-post">
-    <h4>${capitalize(post.title)} by User ${post.userid}</h4>
-    <p>${capitalize(post.post)}</p>
-  </div>`;
-}
 const kelvinToFahrenheit = kelvinTemp =>
   Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
