5be70795328955ab5041f6c8b327b977e864186a
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-04 21:55:50 -0600

    img gallery

--- components/views/Blog.js
+++ components/views/Blog.js
@@ -9,6 +9,8 @@ export default st => `
   <a href="">"Sign up for alerts" "Button"</a>
   <br>
   <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/localhost%20new%20user%20server.png?raw=true"></a>
+  <br>
+  <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/localhost%20userlist.png?raw=true"></a>
 </section>`;
 
 const kelvinToFahrenheit = kelvinTemp =>
