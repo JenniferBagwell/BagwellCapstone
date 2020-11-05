58f029dc775939e6d71245e50f4d700618d37186
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-04 18:52:00 -0600

    fix

--- store/Gallery.js
+++ store/Gallery.js
@@ -2,9 +2,13 @@ export default {
   header: "Gallery",
   view: "Gallery",
   pictures: [
-const img = new Image();
-img.onload = function() {
-  alert(500px + 'x' + 250px);
-}
-img.src = 'https://cdn.filestackcontent.com/KHQdRsg4TTmFRX8WYSQ9';
+    {
+      url: "https://cdn.filestackcontent.com/KHQdRsg4TTmFRX8WYSQ9",
+      title: "Agile Cert"
+    },
+    {
+      url: "img url here",
+      title: "to be added"
+    }
   ]
+};
