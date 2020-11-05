5ee5d7221db17e623b8c89252da7135567ef66b2
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-04 22:58:33 -0600

    video

--- components/views/Home.js
+++ components/views/Home.js
@@ -5,13 +5,13 @@ export default () => `
 <head>
 <!-- The video -->
 <video autoplay muted loop id="myVideo">
-  <source src="imgs\background bio.mp4" type="video/mp4">
+  <source src="imgs/background bio.mp4" type="video/mp4">
 </video>
 
 <!-- Optional: some overlay text to describe the video -->
 <div class="content">
   <h1>Heading</h1>
-  <p>WELCOME</p>
+  <p>Lorem ipsum...</p>
   <!-- Use a button to pause/play the video with JavaScript -->
   <button id="myBtn" onclick="myFunction()">Pause</button>
 </div>
