afb8be3613514085522be4dcfd5aa4a6610a9c69
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-04 22:03:13 -0600

    gallery img

--- components/views/Gallery.js
+++ components/views/Gallery.js
@@ -1,6 +1,5 @@
 export default st => `
 ${st.pictures.reduce(
-  (html, pic) =>
-    html + `<img src="${pic.url}" alt="${pic.title}"width="400" height="250">`,
+  (html, pic) => html + `<img src="${pic.url}" alt="${pic.title}>`,
   ``
 )}`;
