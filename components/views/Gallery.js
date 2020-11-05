f9cd10625ee62fb780f77ce8f180507723106b68
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-04 19:25:30 -0600

    size

--- components/views/Gallery.js
+++ components/views/Gallery.js
@@ -1,6 +1,7 @@
 export default st => `
 <section id="csstransfer">
 ${st.pictures.reduce(
-  (html, pic) => html + `<img src="${pic.url}" alt="${pic.title}">`,
+  (html, pic) =>
+    html + `<img src="${pic.url}" alt="${pic.title}"width="500" height="600">`,
   ``
 )}`;
