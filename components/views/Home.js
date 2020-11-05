e279c557b37f0e23f677f52efbd0a6bbfb6e59d6
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-04 23:14:31 -0600

    banner

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,6 +1,10 @@
 export default () => `
 <section id="csstransfer">
   <h2>ProCode Bio</h2>
+  <section class="slider parallax bg1">
+  <video playsinline autoplay muted loop poster="imgs\procodebackground.png" id="bgvid">
+<source src="imgs\background bio.mp4" type="video/mp4">
+</video>
   <iframe width="560" height="315" src="https://www.youtube.com/embed/c6KtfHEAScc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   <h2>More To Come</h2>
 </section>`;
