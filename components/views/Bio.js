b7595009aca8c72509dfe7bfed71f6a4acbb11b4
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-04 22:21:35 -0600

    video embed

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -5,7 +5,7 @@ export default () => `
     <body>
 <h2 class="white">Professional Bio Linkhub</h2>
 <br>
-<p>This is my professional savvycoders bio. From you can you access my savvycoder profiles through the links provided. There is also a list of all my currant professional Certifications. This professional coder bio will be updated as I continue to build my coding career.</p>
+<p>This is my professional savvycoders bio. From here can you access my savvycoder profiles through the links provided. There is also a list of all my currant professional Certifications. This professional coder bio will be updated as I continue to build my coding career.</p>
 <h3>My professional coding links</h3>
 <ol><h4>
 <li><a href="https://www.linkedin.com/in/jenniferbagwell808/"> My LinkedIn </a></li>
@@ -13,13 +13,20 @@ export default () => `
 <li><a href="https://www.supportingstrategies.com/bootcamp-slm19-jennifer-bagwell">Bootcamp Session</li>
   <li><a href="https://github.com/JenniferBagwell"> My GitHub </a></li>
   <li><a href="https://trello.com/b/yCWNgWGD/bagwell-capstone-kanban"> My Trello Board </a></li>
-  <li><a href="https://github.com/JenniferBagwell"> Previous Contract Work </a></li>
+  <li><a href="https://explanationpt.com/contract-projects"> Previous Contract Work </a></li>
 
   <p>
   <a href="https://cdn2.hubspot.net/hubfs/408309/Bootcamp_SLM-19/Presentations/SPS_BBC_SLM19_Presentation_Bagwell,%20Jennifer.pdf">Free online Social Media Bootcamp Program</a>
   </p>
   </h4>
 </ol>
+<table>
+  <tr>
+    <td>  <iframe width="560" height="315" src="https://www.youtube.com/embed/WJ1VoZlMbNg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a></td>
+    <td>  <iframe width="560" height="315" src="https://www.youtube.com/embed/7o16TYUXgqc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+  </tr>
+</table>
+
   </body>
 </p>
   </section>`;
