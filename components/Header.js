ce6e290623d76d011f4c916ad87d01f02a0ffcf0
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-07 16:21:12 -0600

    header css

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export default st => `
 <header>
 <h2><img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/logo%20header.png?raw=true"></a>${st.header}</h2>
-<h2><p>ProCode.Bio</p></h2>
+<p>ProCode.Bio</p>
   </header>
   `;
