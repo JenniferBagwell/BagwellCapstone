4962f00a36fd8d799319b2e48801a1fc08cd8073
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-07 15:52:02 -0600

    logo fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default () => `
 <footer>
   &copy; 2020 Jennifer Bagwell</a>
-  <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/logo.png"</img>
+  <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/logo.png?raw=trueng"</a>
 </footer>
 `;
