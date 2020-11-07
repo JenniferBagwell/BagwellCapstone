79634ccc4c7111d0f59b5ccbb3ecc48596af05fd
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-07 16:33:53 -0600

    header changes

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export default st => `
 <header>
 <h2>ProCode.Bio</h2>
-<p><img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/logo%20header.png?raw=true"></a>${st.header}</p>
+<p>${st.header}</p>
   </header>
   `;
