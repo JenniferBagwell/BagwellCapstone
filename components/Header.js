df76172dfd599c19980b355db54a4b1b861228d4
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-07 15:59:36 -0600

    header logo

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export default st => `
 <header>
   <h2>${st.header}</h2>
-  <br><img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/logo.png?raw=true"></a>
+  <br><img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/logo%20header.png?raw=true"></a>
   </header>
   `;
