349e28c59c310300de161dd8678d6d0ddf42e4cc
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-10 20:48:59 -0600

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,18 @@
 export default () => `
 <footer>
+
+<div class="footer-container">
+<div class="option">
+<p>
   &copy; 2020 Jennifer Bagwell</a>
   <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/logo.png?raw=trueng"</a>
+  </p>
+  </div>
+
+  <div class="option">
+  <i class="Socal Hub"></i>
   <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/fblogo.png?raw=trueng"</a>
   <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/lnlogo.png?raw=trueng"</a>
-
+  </div>
 </footer>
 `;
