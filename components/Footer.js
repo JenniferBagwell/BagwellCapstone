34aa64a5b5bfea65dcb15860ae24348d4aedb6c5
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-10 20:37:16 -0600

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,5 +2,8 @@ export default () => `
 <footer>
   &copy; 2020 Jennifer Bagwell</a>
   <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/logo.png?raw=trueng"</a>
+  <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/fblogo.png?raw=trueng"</a>
+  <img src="https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/lnlogo.png?raw=trueng"</a>
+
 </footer>
 `;
