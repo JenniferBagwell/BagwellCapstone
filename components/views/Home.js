2f91e89e371a9f17ecc494ace70fd47be4d9e135
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-10 22:13:48 -0600

    div max

--- components/views/Home.js
+++ components/views/Home.js
@@ -5,7 +5,7 @@ export default () => `
   <h2>Welcome to ProCode.bio</h2>
   <p>Here to provide you with one site for all your dev links and information.
   <br>
-  No matter where you are as a deveoperer you will always be asked for links to certain tools and information. After sending link after link, have you ever wished you could just send one link and that link had all the information needed. Now there is a site to do just that. A site for you and your deveoper needs.</p>
+  No matter where you are as a developer you will always be asked for links to certain tools and information. After sending link after link, have you ever wished you could just send one link and that link had all the information needed. Now there is a site to do just that. A site for you and your developer needs.</p>
 </div>
 <br>
 <div>
