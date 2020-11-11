0bcaea14bc4e4593d51c513cd1cb8afeb005a7b9
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-10 21:27:13 -0600

    home page

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,6 +1,6 @@
 export default () => `
 <section id="csstransfer">
   <h2>ProCode Bio</h2>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/c6KtfHEAScc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/2OOwBnXZeXs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   <h2>More To Come</h2>
 </section>`;
