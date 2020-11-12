06cb78e9300a3590d762f31f069c9b3edafe6891
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-11 19:54:28 -0600

    img

--- store/Gallery.js
+++ store/Gallery.js
@@ -12,5 +12,10 @@ export default {
         "https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/hubspot%20ac.png?raw=true",
       title: "Social Media Marketing Certification"
     }
+    {
+      url:
+        "https://github.com/JenniferBagwell/BagwellCapstone/blob/master/imgs/certificate-84779373.jpg?raw=true",
+      title: "Agile Cert"
+    },
   ]
 };
