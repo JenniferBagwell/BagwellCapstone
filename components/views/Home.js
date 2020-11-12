b3ec6bfc1f03540158e32a61be24186993937511
JenniferBagwell <jdawbagwell@gmail.com>
2020-11-12 13:28:41 -0600

    home page css

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,17 +1,14 @@
 export default () => `
 <section id="csstransfer">
 <body>
-<div class="myDiv">
-  <h2>Welcome to ProCode.bio</h2>
-  <p>Here to provide you with one site for all your dev links and information.
-  <br>
-  No matter where you are as a developer you will always be asked for links to certain tools and information. After sending link after link, have you ever wished you could just send one link and that link had all the information needed. Now there is a site to do just that. A site for you and your developer needs.</p>
-</div>
-<br>
-<div>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/7V_mhvASc6g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="myDiv">
+
+  <div>
+  <p class="homedes">Here to provide you with one site for all your dev links and information.</p>
   </div>
-<p>Much more coming soon and fast!</p>
+</div>
+
+  <iframe width="460" height="315" class="youtubevid" src="https://www.youtube.com/embed/7V_mhvASc6g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 </body>
 </section>`;
